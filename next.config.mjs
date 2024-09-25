81d0e4b6088786c24a7648c52c40b8c5f5b0e920
Dakota Fabro <dakota@weareoutright.com>
2024-09-24 19:54:34 -0700

    config for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 import { withFaust, getWpHostname } from "@faustwp/core";
 import { createSecureHeaders } from "next-secure-headers";
-
-require("dotenv").config({
-  path: path.resolve(process.cwd(), ".env.local"),
-});
 /**
  * @type {import('next').NextConfig}
  **/
 export default withFaust({
-  // output: "standalone",
+  output: "standalone",
+  distDir: "out",
   reactStrictMode: true,
   sassOptions: {
     includePaths: ["node_modules"],
