eba7256b7b8f28657ff9a408a356dd3acaa18e26
Dakota Fabro <dakota@weareoutright.com>
2024-10-30 09:27:41 -0700

    set up sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,31 @@
+const FRONTEND_URL = "https://wpadc-archive.vercel.app/";
+
+const siteUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : FRONTEND_URL;
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      //   {
+      //     userAgent: "*",
+      //     disallow: "/thank-you", // Disallow a specific folder/file
+      //   },
+      {
+        userAgent: "*",
+        allow: "/", // Allow all pages by default
+      },
+    ],
+    additionalSitemaps: [
+      //   `${siteUrl}/sitemap-blog.xml`,
+      //   `${siteUrl}/sitemap-posts.xml`,
+    ],
+  },
+  exclude: [
+    "/exclude-file", // for specific page/file
+    "/exclude/*", // for Folder
+  ],
+};
