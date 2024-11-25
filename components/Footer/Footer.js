cdb33a7852000b81fd2b723f071f4ecdb95d9efb
Dakota Fabro <dakota@weareoutright.com>
2024-11-25 15:51:18 -0800

    implement footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,10 @@
 import classNames from "classnames/bind";
-import { Container, NavigationMenu } from "../../components";
+// import { Container, NavigationMenu } from "../../components";
 import styles from "./Footer.module.scss";
+import WPA_FOOTER_LOGO from "../../assets/footer/wpa-footer-logo.svg";
+import WPA_IG from "../../assets/footer/ig-icon.svg";
+import WPA_VIMEO from "../../assets/footer/vimeo-icon.svg";
+import Image from "next/image";
 
 let cx = classNames.bind(styles);
 
@@ -9,10 +13,27 @@ export default function Footer({ title, menuItems, currentPage }) {
 
   return (
     <footer className={cx("component")}>
-      <Container>
-        <NavigationMenu menuItems={menuItems} currentPage={currentPage} />
-        <p className={cx("copyright")}>{`${title} © ${year}`}</p>
-      </Container>
+      {/* <NavigationMenu menuItems={menuItems} currentPage={currentPage} /> */}
+      <div className={cx("left-footer")}>
+        <a className={cx("newsletter-btn")}>Join Our Newsletter</a>
+        <div className={cx("footer-logo")}>
+          <a href="#">
+            <Image src={WPA_FOOTER_LOGO} alt="Home" />
+          </a>
+          <p>Washington, DC | info@wpadc.org</p>
+        </div>
+      </div>
+      <div className={cx("right-footer")}>
+        <div className={cx("footer-social-icons")}>
+          <a href="#">
+            <Image src={WPA_IG} alt="WPA - Instagram" />
+          </a>
+          <a href="#">
+            <Image src={WPA_VIMEO} alt="WPA - Vimeo" />
+          </a>
+        </div>
+        <p className={cx("copyright")}>© All rights reserved | {year}</p>
+      </div>
     </footer>
   );
 }
