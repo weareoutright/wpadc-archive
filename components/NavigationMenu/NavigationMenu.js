bcc88e40448a5474a807b513f5d00e93e50e6e2e
Dakota Fabro <dakota@weareoutright.com>
2025-03-03 11:50:24 -0800

    adjust size of wpa logo in nav

--- components/NavigationMenu/NavigationMenu.js
+++ components/NavigationMenu/NavigationMenu.js
@@ -1,7 +1,6 @@
 import classNames from "classnames/bind";
 import { gql } from "@apollo/client";
 import styles from "./NavigationMenu.module.scss";
-import stylesFromWP from "./NavigationMenuClassesFromWP.module.scss";
 import { flatListToHierarchical } from "@faustwp/core";
 import Image from "next/image";
 import MENU_TOGGLE_BTN from "../../assets/header/block-menu.svg";
