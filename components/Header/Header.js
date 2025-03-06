7d44d02e0e634859e9462ae74659fbc6b9ebf79b
Dakota Fabro <dakota@weareoutright.com>
2025-03-06 13:41:46 -0800

    fix z-index of header and nav

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -34,85 +34,93 @@ export default function Header({
   const isPeoplePage = currentRoute === "/people";
 
   return (
-    <header className={cx("component", isFrontPage && "front-page-component")}>
-      <SkipNavigationLink />
-      {isNavShown && <FullPageNav setIsNavShown={setIsNavShown} />}
-      <Container
-        className={isFrontPage && containerCx("front-page-header-container")}
+    <>
+      <header
+        className={cx("component", isFrontPage && "front-page-component")}
       >
-        <div className={isFrontPage ? cx("front-page-navbar") : cx("navbar")}>
-          <div className={isFrontPage ? cx("front-page-brand") : cx("brand")}>
-            <Link href="/">
-              <a className={isFrontPage ? cx("front-page-title") : cx("title")}>
-                <Image src={LOGO} alt="Home" />
-              </a>
-            </Link>
-            <Link href="/">
-              <a
-                className={
-                  isFrontPage
-                    ? cx("front-page-sub-logo-title")
-                    : cx("sub-logo-title")
-                }
-              >
-                <Image src={WASHINGTON_PROJECT} alt="Home" />
-                <Image src={FOR_THE_ARTS} alt="Home" />
-              </a>
-            </Link>
+        <SkipNavigationLink />
+        {isNavShown && <FullPageNav setIsNavShown={setIsNavShown} />}
+        <Container
+          className={isFrontPage && containerCx("front-page-header-container")}
+        >
+          <div className={isFrontPage ? cx("front-page-navbar") : cx("navbar")}>
+            <div className={isFrontPage ? cx("front-page-brand") : cx("brand")}>
+              <Link href="/">
+                <a
+                  className={isFrontPage ? cx("front-page-title") : cx("title")}
+                >
+                  <Image src={LOGO} alt="Home" />
+                </a>
+              </Link>
+              <Link href="/">
+                <a
+                  className={
+                    isFrontPage
+                      ? cx("front-page-sub-logo-title")
+                      : cx("sub-logo-title")
+                  }
+                >
+                  <Image src={WASHINGTON_PROJECT} alt="Home" />
+                  <Image src={FOR_THE_ARTS} alt="Home" />
+                </a>
+              </Link>
 
-            {description && <p className={cx("description")}>{description}</p>}
-          </div>
-          <button
-            type="button"
-            className={
-              isFrontPage ? cx("front-page-nav-toggle") : cx("nav-toggle")
-            }
-            onClick={() => setIsNavShown(!isNavShown)}
-            aria-label="Toggle navigation"
-            aria-controls={cx("primary-navigation")}
-            aria-expanded={isNavShown}
-          >
-            <Image src={NAV_MENU_BTN} alt="open nav" />
-          </button>
-          <NavigationMenu
-            className={cx([
-              "primary-navigation",
-              isNavShown ? "show" : undefined,
-              isFrontPage ? "front-page-primary-nav" : undefined,
-            ])}
-            menuItems={menuItems}
-            isNavShown={isNavShown}
-            setIsNavShown={setIsNavShown}
-          />
-        </div>
-        {isFrontPage && (
-          <div className={cx("front-page-logo")}>
-            <Image
-              src={FRONT_PAGE_LOGO}
-              alt="Washington Project of the Arts Logo"
+              {description && (
+                <p className={cx("description")}>{description}</p>
+              )}
+            </div>
+            <button
+              type="button"
+              className={
+                isFrontPage ? cx("front-page-nav-toggle") : cx("nav-toggle")
+              }
+              onClick={() => setIsNavShown(!isNavShown)}
+              aria-label="Toggle navigation"
+              aria-controls={cx("primary-navigation")}
+              aria-expanded={isNavShown}
+            >
+              <Image src={NAV_MENU_BTN} alt="open nav" />
+            </button>
+            <NavigationMenu
+              className={cx([
+                "primary-navigation",
+                isNavShown ? "show" : undefined,
+                isFrontPage ? "front-page-primary-nav" : undefined,
+              ])}
+              menuItems={menuItems}
+              isNavShown={isNavShown}
+              setIsNavShown={setIsNavShown}
             />
           </div>
+          {isFrontPage && (
+            <div className={cx("front-page-logo")}>
+              <Image
+                src={FRONT_PAGE_LOGO}
+                alt="Washington Project of the Arts Logo"
+              />
+            </div>
+          )}
+          {isFrontPage && <SearchBar isFrontPage={isFrontPage} />}
+          {isFrontPage && (
+            <a
+              href="#front-page-featured"
+              className={cx("front-page-down-waypoint")}
+            >
+              <Image src={DOWN_WAYPOINT} alt={"continue to next section"} />
+            </a>
+          )}
+        </Container>
+        {isAboutPage && (
+          <h1 className={cx(["full-width-page-header", "vermillion-bg"])}>
+            <span>About The Archive</span>
+          </h1>
         )}
-        {isFrontPage && <SearchBar isFrontPage={isFrontPage} />}
-        {isFrontPage && (
-          <a
-            href="#front-page-featured"
-            className={cx("front-page-down-waypoint")}
-          >
-            <Image src={DOWN_WAYPOINT} alt={"continue to next section"} />
-          </a>
+        {isPeoplePage && (
+          <h1 className={cx(["full-width-page-header", "indigo-bg"])}>
+            <span>People</span>
+          </h1>
         )}
-      </Container>
-      {isAboutPage && (
-        <h1 className={cx(["full-width-page-header", "vermillion-bg"])}>
-          <span>About The Archive</span>
-        </h1>
-      )}
-      {isPeoplePage && (
-        <h1 className={cx(["full-width-page-header", "indigo-bg"])}>
-          <span>People</span>
-        </h1>
-      )}
-    </header>
+      </header>
+    </>
   );
 }
