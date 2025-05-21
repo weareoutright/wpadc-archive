e38f0413a854b3330f103c0186d29e7c819172a2
Dakota Fabro <dakota@weareoutright.com>
2025-05-21 12:12:54 -0700

    include person image in link

--- components/PersonSearchResultCard/PersonSearchResultCard.js
+++ components/PersonSearchResultCard/PersonSearchResultCard.js
@@ -8,26 +8,27 @@ const PersonSearchResultCard = ({ node }) => {
   console.log(node.node);
   return (
     <div key={node.node.id} className={cx("AssetCard")}>
-      {node.personCard?.personInfo[0].headshot ? (
-        <Image
-          src={node.personCard?.personInfo[0].headshot?.node.sourceUrl}
-          alt={node.personCard?.personInfo[0].headshot?.node.title}
-          className={cx("asset-image")}
-          width={244}
-          height={320}
-          layout="fixed"
-        />
-      ) : (
-        <Image
-          src="./checked-bg-minimal-content.png"
-          alt=""
-          className={cx("asset-image")}
-          width={244}
-          height={320}
-          layout="fixed"
-        />
-      )}
       <a href={node.node.uri} className={cx("asset-link")}>
+        {node.personCard?.personInfo[0].headshot ? (
+          <Image
+            src={node.personCard?.personInfo[0].headshot?.node.sourceUrl}
+            alt={node.personCard?.personInfo[0].headshot?.node.title}
+            className={cx("asset-image")}
+            width={244}
+            height={320}
+            layout="fixed"
+          />
+        ) : (
+          <Image
+            src="./checked-bg-minimal-content.png"
+            alt=""
+            className={cx("asset-image")}
+            width={244}
+            height={320}
+            layout="fixed"
+          />
+        )}
+
         <h3>{node.node.title}</h3>
       </a>
     </div>
